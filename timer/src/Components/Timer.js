db7cedb9324130a6d23de7ebd27e75626150600e
caiobacode <caioodsf@gmail.com>
2022-11-07 17:29:02 -0300

    adding colors

--- timer/src/Components/Timer.js
+++ timer/src/Components/Timer.js
@@ -34,28 +34,40 @@ function Timer() {
   return (
     <div className="main-div">
       <div className="timer">
-      <button type="button" onClick={() => setWasStarted(true)}>Start</button>
+      <button 
+        disabled={wasStarted}
+        className="start-btn" 
+        type="button" 
+        onClick={() => setWasStarted(true)}>Start</button>
       {
         wasStarted ? (
-          <div>
-            <span>{totalTimeinMinutes.toString().padStart(2, '0')}</span>
-            <span>:</span>
-            <span>{seconds.toString().padStart(2, '0')}</span>
+          <div className="running-inputs">
+            <div className="minutes-started">
+              <span>{totalTimeinMinutes.toString().padStart(2, '0')}</span>
+            </div>
+            <span className="two-points-started">:</span>
+            <div className="seconds-started">
+              <span>{seconds.toString().padStart(2, '0')}</span>
+            </div>
           </div>)
           : (
-          <div>
+          <div className="start-inputs">
           <input
+            className="minutes-input"
             type="number"
             onChange={handleChangeMinutes}
             value={totalTimeinMinutes.toString().padStart(2, '0')}></input>
-          <span>:</span>
+          <span className="two-points">:</span>
           <input 
+            className="seconds-input"
             type="number"
             onChange={handleChangeSeconds}
             value={seconds.toString().padStart(2, '0')}></input> 
           </div>)
       }
       <button
+        disabled={!wasStarted}
+        className="finish-btn"
         type="buttun"
         onClick={() => setWasStarted(false)}>Stop</button>
       </div>
