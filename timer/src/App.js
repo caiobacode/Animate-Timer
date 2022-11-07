ddcba06423e3bdb8df68fc173896a8c7639275dc
caiobacode <caioodsf@gmail.com>
2022-11-07 18:58:06 -0300

    revoving footer

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Timer from './Components/Timer';
-import Footer from './Components/Footer';
 import mine from './Media/minecraft.mp4';
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
           position: 'absolute',
           width: '100%',
           left: '50%',
-          top: '50%',
+          top: '59%',
           height: '100%',
           objectFit: 'cover',
           transform: 'translate(-50%, -59%)',
@@ -27,7 +26,6 @@ function App() {
       </video>  
       <Header />
       <Timer />
-      <Footer />
     </div>
   );
 }
